fff225bf42ac94a242043b053e0b17ed5fe5808f
rhelmus <rhelmus@gmail.com>
2015-10-04 19:57:29 +0200

    removed stuff from gh-pages branch

--- src/utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "wrapper_utils.h"
-
-namespace virtmem {
-
-const CNILL NILL = CNILL(); // need empty constructor to prevent compiler error on arduino
-
-}
--- update-docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-inkscape -z  doc/intro-scheme.svg -e doc/intro-scheme.png -d 65
-doxygen doc/Doxyfile
